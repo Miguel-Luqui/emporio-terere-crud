e08c3580895dbbe035d78b0a39f37bc851a445b7
Miguel Santana Luqui <callmerobson10@gmail.com>
2025-09-11 13:29:32 -0300

    Criação da classe "Produto"

---
+++ EmporioTerere.Api/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace EmporioTerere.Api.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
